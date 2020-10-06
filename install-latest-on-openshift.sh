83314b7438ebb3b3d8ac016163304e5367df1fd2
Laurent Broudoux <laurent.broudoux@gmail.com>
2020-10-06 17:22:56 +0200

    Adding script for quick install of latest version on OpenShift

---
+++ install-latest-on-openshift.sh
@@ -0,0 +1,6 @@
+oc create -f deploy/crds/microcks_v1alpha1_microcksinstall_crd.yaml -n microcks 
+oc create -f deploy/service_account.yaml -n microcks  
+oc create -f deploy/role.yaml -n microcks 
+oc create -f deploy/role_binding.yaml -n microcks 
+oc create -f deploy/operator_latest.yaml -n microcks
+oc create -f deploy/crds/openshift-features.yaml -n microcks 
